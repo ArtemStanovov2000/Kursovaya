dcf3f0f8da4510db0ff11453da3c1fedd1b38b47
g7g7g8 <artemstanovov36@gmail.com>
2023-03-07 11:30:13 +0300

    3 расчета калькулятора предварительный

--- dist/script.js
+++ dist/script.js
@@ -1,18 +1,45 @@
-const buttonCurrentConversion = document.getElementById("currentConversion")
+const REMOVING_PREFIX_KILO = 1000;
+const LIMIT_OF_INCREASED_ACCURACY = 10;
 
-const currentConversion = function currentConversion() {
-    let currentInput = document.getElementById("current").value
-    let currentResultInput = document.getElementById("current_result")
+const buttonAmperageConversion = document.getElementById("amperageConversion");
+const buttonVoltageConversion = document.getElementById("voltageConversion");
+const buttonElectricalPowerConversion = document.getElementById("powerConversion");
 
-    let currentResult = currentInput * 1000;
-    if (currentResult < 10) {
-        currentResult = currentResult.toFixed(2);
-        currentResultInput.innerHTML = currentResult;
-    };
-    currentResultInput.innerHTML = currentResult;
-};
+buttonAmperageConversion.addEventListener("click", () => {
+    let amperageValue = document.getElementById("amperage_input").value
+    let amperageResultOutput = document.getElementById("amperage_result")
 
-buttonCurrentConversion.addEventListener("click", () => {
-    currentConversion();
+    let amperageResult = amperageValue * REMOVING_PREFIX_KILO;
+
+    if (amperageResult < LIMIT_OF_INCREASED_ACCURACY) {
+        amperageResultOutput.innerHTML = amperageResult.toFixed(2);
+    } else {
+        amperageResultOutput.innerHTML = amperageResult.toFixed(0);
+    }
 });
 
+buttonVoltageConversion.addEventListener("click", () => {
+    let voltageValue = document.getElementById("voltage_input").value
+    let voltageResultOutput = document.getElementById("voltage_result")
+
+    let voltageResult = voltageValue * REMOVING_PREFIX_KILO;
+
+    if (voltageResult < LIMIT_OF_INCREASED_ACCURACY) {
+        voltageResultOutput.innerHTML = voltageResult.toFixed(2);
+    } else {
+        voltageResultOutput.innerHTML = voltageResult.toFixed(0);
+    }
+});
+
+buttonElectricalPowerConversion.addEventListener("click", () => {
+    let electricalPowerValue = document.getElementById("electrical_power_input").value
+    let electricalPowerResultOutput = document.getElementById("electrical_power_result")
+
+    let electricalPowerResult = electricalPowerValue * REMOVING_PREFIX_KILO;
+
+    if (electricalPowerResult < LIMIT_OF_INCREASED_ACCURACY) {
+        electricalPowerResultOutput.innerHTML = electricalPowerResult.toFixed(2);
+    } else {
+        electricalPowerResultOutput.innerHTML = electricalPowerResult.toFixed(0);
+    }
+});
